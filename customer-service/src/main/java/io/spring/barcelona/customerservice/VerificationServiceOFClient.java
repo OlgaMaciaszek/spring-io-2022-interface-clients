a20b54d05c9b4a734e354fa8d7c961855e70a951
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-05-25 13:00:16 +0200

    Remove unnecessary annotations and dependencies.

--- customer-service/src/main/java/io/spring/barcelona/customerservice/VerificationServiceOFClient.java
+++ customer-service/src/main/java/io/spring/barcelona/customerservice/VerificationServiceOFClient.java
@@ -1,7 +1,6 @@
 package io.spring.barcelona.customerservice;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,11 +8,12 @@ import org.springframework.web.bind.annotation.RequestHeader;
 /**
  * @author Olga Maciaszek-Sharma
  */
+
 @FeignClient("verification-service")
 public interface VerificationServiceOFClient {
 
-	@PostMapping(path = "/verify", consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	CustomerVerificationResult verify(@RequestBody CustomerApplication customerApplication, @RequestHeader("X-Custom-Header") String customHeader);
+	@PostMapping(path = "/verify")
+	CustomerVerificationResult verify(@RequestBody CustomerApplication customerApplication,
+			@RequestHeader("X-Custom-Header") String customHeader);
 
 }
